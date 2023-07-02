56ed5bbac808b3d9cbcd86b209124ce27bcf1bc1
masa1357 <masa1357@mprg.cs.chubu.ac.jp>
2023-07-02 14:55:42 +0900

    complite feature!

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:external_path/external_path.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:http/http.dart' as http;
@@ -31,10 +32,18 @@ class Recorder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Record',
+      title: 'Satori_',
+      theme: ThemeData(
+          scaffoldBackgroundColor: const Color(0xFF203549),
+          textTheme: const TextTheme(
+            bodyLarge: TextStyle(color: Color(0xFFdcc7b3)),
+            bodyMedium:
+                TextStyle(color: Color(0xFFdcc7b3)), // この色がアプリ全体の背景色になります
+          )),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Record'),
+          title: const Text('Satori'),
+          backgroundColor: const Color(0xFF203549),
         ),
         body: const Center(
           child: WindowBody(),
@@ -60,11 +69,11 @@ class _WindowBodyState extends State<WindowBody> {
 
   // API response holder
   Map<String, dynamic> apiResponse = {
-    "calm": 0,
-    "anger": 0,
-    "joy": 0,
-    "sorrow": 0,
-    "energy": 0,
+    "calm": 40,
+    "anger": 50,
+    "joy": 10,
+    "sorrow": 30,
+    "energy": 20,
   };
 
   Future<Map<String, dynamic>> _getAPI(String filePath) async {
@@ -129,10 +138,10 @@ class _WindowBodyState extends State<WindowBody> {
     await record.stop();
     await _convertToWav();
     var response = await _getAPI(newPath);
-    logger.i(response); // or do whatever you want with the response
     setState(() {
       apiResponse = response;
     });
+    logger.i(apiResponse); // or do whatever you want with the response
   }
 
   // void _startPlaying() async {
@@ -181,12 +190,11 @@ class _WindowBodyState extends State<WindowBody> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.all(50.0),
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           children: <Widget>[
-            Text((_flag ? "録音中...\n" : "録音ボタンを押してね！"),
+            Text((_flag ? "今の私の感情は..." : " "),
                 style: const TextStyle(
-                  color: Colors.greenAccent,
                   fontSize: 40.0,
                   fontWeight: FontWeight.w600,
                   fontFamily: "RondeB",
@@ -194,16 +202,92 @@ class _WindowBodyState extends State<WindowBody> {
             TextButton(
                 onPressed: _startTimer,
                 style: TextButton.styleFrom(
-                  foregroundColor: Colors.blue,
-                  backgroundColor: Colors.tealAccent,
+                  foregroundColor: Colors.green,
+                  backgroundColor: Colors.grey,
                   shadowColor: Colors.teal,
                   elevation: 5,
                 ),
                 child: Text((_flag ? "停止" : "開始"),
-                    style: const TextStyle(
-                        color: Color.fromARGB(255, 187, 52, 52),
-                        fontSize: 40.0)))
-                        //ここにチャートを追加
+                    style:
+                        const TextStyle(color: Colors.black, fontSize: 40.0))),
+            Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: AspectRatio(
+                    aspectRatio: 1.3,
+                    child: Stack(alignment: Alignment.center, children: [
+                      Container(
+                        width: 200,
+                        height: 300,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: const Color(0xFFdcc7b3),
+                            width: 2,
+                          ),
+                        ),
+                      ),
+                      RadarChart(
+                        RadarChartData(
+                          dataSets: [
+                            RadarDataSet(
+                              // dataEntriesはRadarEntryのリストで、それぞれがチャート上の点を表します。
+                              // RadarEntryのvalueプロパティは、チャートの中心からの距離を決定します。
+                              dataEntries: [
+                                RadarEntry(
+                                    value: apiResponse["calm"].toDouble()),
+                                RadarEntry(
+                                    value: apiResponse["anger"].toDouble()),
+                                RadarEntry(
+                                    value: apiResponse["joy"].toDouble()),
+                                RadarEntry(
+                                    value: apiResponse["sorrow"].toDouble()),
+                                RadarEntry(
+                                    value: apiResponse["energy"].toDouble()),
+                              ],
+
+                              fillColor: const Color.fromARGB(255, 226, 241, 14)
+                                  .withOpacity(0.2), // set the fill color
+                              borderColor: const Color.fromARGB(
+                                  255, 226, 241, 14), // set the border color
+                              borderWidth: 2.0, // set the border width
+                            ),
+                          ],
+                          radarBackgroundColor:
+                              const Color.fromARGB(0, 240, 62, 62),
+                          borderData: FlBorderData(show: false),
+                          radarBorderData: const BorderSide(
+                              color: Color.fromARGB(0, 250, 17, 17)),
+                          titlePositionPercentageOffset: 0.2,
+                          titleTextStyle: const TextStyle(fontSize: 16),
+                          getTitle: (index) {
+                            switch (index) {
+                              case 0:
+                                return "リラックス";
+                              case 1:
+                                return "怒り";
+                              case 2:
+                                return "楽しみ";
+                              case 3:
+                                return "悲しみ";
+                              case 4:
+                                return "元気";
+                              default:
+                                return "";
+                            }
+                          },
+                          tickCount: 1,
+                          ticksTextStyle: const TextStyle(
+                              color: Colors.transparent, fontSize: 10),
+                          tickBorderData:
+                              const BorderSide(color: Colors.transparent),
+                          gridBorderData: const BorderSide(
+                              color: Color(0xFFdcc7b3), width: 2),
+                        ),
+                        swapAnimationDuration:
+                            const Duration(milliseconds: 200),
+                        swapAnimationCurve: Curves.linear,
+                      )
+                    ])))
           ],
         ));
   }
